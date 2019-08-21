9ea256d5ac2f3f5542ad2949ec859eb8377b10ed
anire3479 <anire.bowman@gmail.com>
2019-08-21 14:05:15 -0400

    moving link to const

--- react-client/src/Layouts/ProjectsPage/ProjectsPage.jsx
+++ react-client/src/Layouts/ProjectsPage/ProjectsPage.jsx
@@ -7,14 +7,15 @@ import './style.css'
 
 const DRIFT_DESCRIPTION = "Drift Chat/Video Onboarding"
 const EMOTION_DESCRIPTION = "e-Motion 112 Term Project"
-
+const DRIFT_DESCRIPTION_VIDEO = "https://youtu.be/VjrM9s5cKos"
+const EMOTION_DESCRIPTION_VIDEO = "https://youtu.be/nrJjACS6eC8"
 
 class ProjectsPage extends Component {
   render() {
     return(
       <div className="pageComponents">
-        <Card img={driftOnboarding} description={DRIFT_DESCRIPTION} link="https://youtu.be/VjrM9s5cKos"/>
-        <Card img={emotionProject} description={EMOTION_DESCRIPTION} link="https://youtu.be/nrJjACS6eC8"/>
+        <Card img={driftOnboarding} description={DRIFT_DESCRIPTION} link={DRIFT_DESCRIPTION_VIDEO}/>
+        <Card img={emotionProject} description={EMOTION_DESCRIPTION} link={EMOTION_DESCRIPTION_VIDEO}/>
       </div>
     )
   }
