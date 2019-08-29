9f2c3fb60e115d01272e8d322c8fdbcb36a31807
anire3479 <anire.bowman@gmail.com>
2019-08-29 00:28:39 -0400

    removed asset

--- react-client/src/Layouts/HomePage/HomePage.jsx
+++ react-client/src/Layouts/HomePage/HomePage.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import studentPicture from '../../Assets/student_picture.jpeg'
-import artPicture from '../../Assets/art_picture.jpg'
-
 import './style.css'
 
 class HomePage extends Component {
