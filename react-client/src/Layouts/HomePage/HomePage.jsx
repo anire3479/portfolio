9d93f9d2bc8bd4f3d23a26068270699038a7098d
anire3479 <anire.bowman@gmail.com>
2019-08-29 00:27:15 -0400

    removed image

--- react-client/src/Layouts/HomePage/HomePage.jsx
+++ react-client/src/Layouts/HomePage/HomePage.jsx
@@ -12,10 +12,6 @@ class HomePage extends Component {
           <img src={studentPicture}></img>
           <span>A software engineer learning something new every day. A big proponent of extreme ownership and becoming stronger from your mistakes. Focused on cultivating knowledge in order to pave a path for generations to come.</span>
         </div>
-        <div>
-          <span>I see this website as not only a personal portfolio but a gateway to my thoughts and learnings. Can’t wait to see what lies ahead next. Can’t wait to show you.</span>
-          <img src={artPicture}></img>
-        </div>
         <span className="quote">"My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style." - Maya Angelou</span>
       </div>
     )
